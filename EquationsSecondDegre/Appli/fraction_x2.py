c3f6aaead7bef8675167b3370ee55d25e7648e2b
rfoxinter <51450425+rfoxinter@users.noreply.github.com>
2020-10-28 21:05:44 +0100

    Update fraction_x2.py

--- EquationsSecondDegre/Appli/fraction_x2.py
+++ EquationsSecondDegre/Appli/fraction_x2.py
@@ -1,7 +1,6 @@
 from tkinter import *
-import matplotlib.pyplot as plt
-import numpy as np
 import __main__
 
 def x2_frac(a_num,a_denom,b_num,b_denom,delta_num,delta_denom):
-    print('racine 2')
+    L5=Label(__main__.Fen,text='x\u2082='+str(((-b_num*a_denom*delta_denom**0.5)+(b_denom*a_denom*delta_num**0.5))/(b_denom*2*a_num*delta_denom**0.5)))
+    L5.grid(column=6,row=4,sticky='w')
