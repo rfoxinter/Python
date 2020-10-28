382b5a9846c0d0f6747b50a816cd33b600a1d73e
rfoxinter <51450425+rfoxinter@users.noreply.github.com>
2020-10-28 21:05:35 +0100

    Create fraction_pas_racine.py

---
+++ EquationsSecondDegre/Appli/fraction_pas_racine.py
@@ -0,0 +1,41 @@
+from tkinter import *
+import matplotlib.pyplot as plt
+import numpy as np
+import __main__
+
+def main(a_num,a_denom,b_num,b_denom,c_num,c_denom,alpha_num,alpha_denom,beta_num,beta_denom):
+    L4=Label(__main__.Fen,text='L\u2019\u00E9quation n\u2019admet pas de racine')
+    L4.grid(column=6,row=3,sticky='w')
+    pltxmin=alpha_num/alpha_denom-10
+    pltxmax=alpha_num/alpha_denom+10
+    ymax=0
+    if (a_num/a_denom)*(pltxmin)**2+(b_num/b_denom)*(pltxmin)+(c_num/c_denom)>(a_num/a_denom)*(pltxmax)**2+(b_num/b_denom)*(pltxmax)+(c_num/c_denom):
+        if (a_num/a_denom)*(pltxmin)**2+(b_num/b_denom)*(pltxmin)+(c_num/c_denom)>0:
+            ymax=(a_num/a_denom)*(pltxmin)**2+(b_num/b_denom)*(pltxmin)+(c_num/c_denom)
+        else:
+            ymax=(a_num/a_denom)*(pltxmax)**2+(b_num/b_denom)*(pltxmax)+(c_num/c_denom)
+    else:
+        if (a_num/a_denom)*(pltxmax)**2+(b_num/b_denom)*(pltxmax)+(c_num/c_denom)>0:
+            ymax=(a_num/a_denom)*(pltxmax)**2+(b_num/b_denom)*(pltxmax)+(c_num/c_denom)
+        else:
+            ymax=(a_num/a_denom)*(pltxmin)**2+(b_num/b_denom)*(pltxmin)+(c_num/c_denom)
+    x=np.linspace(pltxmin,pltxmax,1000)
+    y=(a_num/a_denom)*x**2+(b_num/b_denom)*x+(c_num/c_denom)
+    __main__.plt.plot(x,y,c='blue')
+    __main__.plt.scatter(alpha_num/alpha_denom,beta_num/beta_denom,c='red',marker='x',label="(\u03B1;\u03B2)")
+    __main__.plt.xlabel("x")
+    __main__.plt.ylabel("y=f(x)")
+    pltymin=0
+    pltymax=0
+    if beta_num/beta_denom-ymax/100<ymax:
+        pltymin=beta_num/beta_denom-ymax/100
+        pltymax=ymax
+    else:
+        pltymin=ymax
+        pltymax=beta_num/beta_denom-ymax/100
+    __main__.plt.axis([pltxmin,pltxmax,pltymin,pltymax])
+    __main__.plt.axhline(y=0,c='black')
+    __main__.plt.axvline(x=0,c='black')
+    __main__.plt.legend()
+    __main__.plt.title('f(x)=('+str(a_num)+')/('+str(a_denom)+')x\u00B2+('+str(b_num)+')/('+str(b_denom)+')x+('+str(c_num)+')/('+str(c_denom)+')')
+    __main__.plt.show()
