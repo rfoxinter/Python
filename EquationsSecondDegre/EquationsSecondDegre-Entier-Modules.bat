b5ea72adc6ab738d6d4eed9d9c1be739c5ec1159
rfoxinter <51450425+rfoxinter@users.noreply.github.com>
2020-11-02 19:34:33 +0100

    Update EquationsSecondDegre-Entier-Modules.bat

--- EquationsSecondDegre/EquationsSecondDegre-Entier-Modules.bat
+++ EquationsSecondDegre/EquationsSecondDegre-Entier-Modules.bat
@@ -1,3 +1,3 @@
 python -m pip install --upgrade pip
-python -m pip install --upgrade numpy
-python -m pip install --upgrade matplotlib
+python -m pip install --upgrade pip install numpy==1.19.3
+python -m pip install --upgrade pip install matplotlib==3.3.2
