b5df9295856f7e597fef3346025597e4fef49709
rfoxinter <51450425+rfoxinter@users.noreply.github.com>
2020-12-05 10:28:52 +0100

    Update fraction.py

--- EquationsSecondDegre/Appli/fraction.py
+++ EquationsSecondDegre/Appli/fraction.py
@@ -8,10 +8,10 @@ import fraction_deux_racines_plt
 
 def main_frac():
     __main__.plt.close()
-    for label in __main__.Fen.grid_slaves():
+    for label in __main__.root.grid_slaves():
         if int(label.grid_info()["column"])==6:
             label.destroy()
-    Col7=Label(__main__.Fen,width=100)
+    Col7=Label(__main__.root,width=100)
     Col7.grid(column=6,row=5)
     sec_frac(__main__.ValeurA_n.get(),__main__.ValeurA_d.get(),__main__.ValeurB_n.get(),__main__.ValeurB_d.get(),__main__.ValeurC_n.get(),__main__.ValeurC_d.get())
 
@@ -28,7 +28,7 @@ def sec_frac(a_num,a_denom,b_num,b_denom,c_num,c_denom):
     if delta_num%delta_denom==0:
         delta_num=delta_num//delta_denom
         delta_denom=1
-        L1=Label(__main__.Fen,text='\u0394='+str(delta_num))
+        L1=Label(__main__.root,text='\u0394='+str(delta_num))
         L1.grid(column=6,row=0,sticky='w')
     else:
         div_delta=2
@@ -39,14 +39,14 @@ def sec_frac(a_num,a_denom,b_num,b_denom,c_num,c_denom):
             else:
                 div_delta=div_delta+1
         if delta_num<0 and delta_denom<0:
-            L1=Label(__main__.Fen,text='\u0394='+str(int(-delta_num))+')/('+str(int(-delta_denom))+')='+str(delta_num/delta_denom))
+            L1=Label(__main__.root,text='\u0394='+str(int(-delta_num))+')/('+str(int(-delta_denom))+')='+str(delta_num/delta_denom))
             L1.grid(column=6,row=0,sticky='w')
         else:
-            L1=Label(__main__.Fen,text='\u0394=('+str(int(delta_num))+')/('+str(int(delta_denom))+')='+str(delta_num/delta_denom))
+            L1=Label(__main__.root,text='\u0394=('+str(int(delta_num))+')/('+str(int(delta_denom))+')='+str(delta_num/delta_denom))
             L1.grid(column=6,row=0,sticky='w')
     if alpha_num%alpha_denom==0:
         alpha=alpha_num//alpha_denom
-        L2=Label(__main__.Fen,text='\u03B1='+str(alpha))
+        L2=Label(__main__.root,text='\u03B1='+str(alpha))
         L2.grid(column=6,row=1,sticky='w')
     else:
         div_alpha=2
@@ -57,14 +57,14 @@ def sec_frac(a_num,a_denom,b_num,b_denom,c_num,c_denom):
             else:
                 div_alpha=div_alpha+1
         if alpha_num<0 and alpha_denom<0:
-            L2=Label(__main__.Fen,text='\u03B1=('+str(int(-alpha_num))+')/('+str(int(-alpha_denom))+')='+str(alpha_num/alpha_denom))
+            L2=Label(__main__.root,text='\u03B1=('+str(int(-alpha_num))+')/('+str(int(-alpha_denom))+')='+str(alpha_num/alpha_denom))
             L2.grid(column=6,row=1,sticky='w')
         else:
-            L2=Label(__main__.Fen,text='\u03B1=('+str(int(alpha_num))+')/('+str(int(alpha_denom))+')='+str(alpha_num/alpha_denom))
+            L2=Label(__main__.root,text='\u03B1=('+str(int(alpha_num))+')/('+str(int(alpha_denom))+')='+str(alpha_num/alpha_denom))
             L2.grid(column=6,row=1,sticky='w')
     if beta_num%beta_denom==0:
         beta=beta_num//beta_denom
-        L3=Label(__main__.Fen,text='\u03B2='+str(beta))
+        L3=Label(__main__.root,text='\u03B2='+str(beta))
         L3.grid(column=6,row=2,sticky='w')
     else:
         div_beta=2
@@ -75,10 +75,10 @@ def sec_frac(a_num,a_denom,b_num,b_denom,c_num,c_denom):
             else:
                 div_beta=div_beta+1
         if beta_num<0 and beta_denom<0:
-            L3=Label(__main__.Fen,text='\u03B2=('+str(int(-beta_num))+')/('+str(int(-beta_denom))+')='+str(beta_num/beta_denom))
+            L3=Label(__main__.root,text='\u03B2=('+str(int(-beta_num))+')/('+str(int(-beta_denom))+')='+str(beta_num/beta_denom))
             L3.grid(column=6,row=2,sticky='w')
         else:
-            L3=Label(__main__.Fen,text='\u03B2=('+str(int(beta_num))+')/('+str(int(beta_denom))+')='+str(beta_num/beta_denom))
+            L3=Label(__main__.root,text='\u03B2=('+str(int(beta_num))+')/('+str(int(beta_denom))+')='+str(beta_num/beta_denom))
             L3.grid(column=6,row=2,sticky='w')
     if delta_num<0:
         fraction_pas_racine.main(a_num,a_denom,b_num,b_denom,c_num,c_denom,alpha_num,alpha_denom,beta_num,beta_denom)
