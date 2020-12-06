54d8c6308ccd8c7e1522c1248ad3e014b547faff
rfoxinter <51450425+rfoxinter@users.noreply.github.com>
2020-12-06 15:04:33 +0100

    Update main_maj.py

--- EquationsSecondDegre/Appli/main_maj.py
+++ EquationsSecondDegre/Appli/main_maj.py
@@ -1,6 +1,6 @@
 import urllib.request
 
-filename=['maj','main']
+filename=['__main__','maj']
 for i in range(len(filename)):
 	url='https://raw.githubusercontent.com/rfoxinter/Python/master/EquationsSecondDegre/Appli/'+filename[i]+'.py'
 	urllib.request.urlretrieve(url, filename[i]+'.py')
