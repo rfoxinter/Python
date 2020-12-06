af6bf2e1751bf95a4689b558b30a940e19099a21
rfoxinter <51450425+rfoxinter@users.noreply.github.com>
2020-12-06 14:56:47 +0100

    Update maj.py

--- EquationsSecondDegre/Appli/maj.py
+++ EquationsSecondDegre/Appli/maj.py
@@ -1,4 +1,5 @@
 import urllib.request
+import __main__
 
 filename=['entier','entier_deux_racines_plt','entier_pas_racine','entier_x0','entier_x1','entier_x2','fraction','fraction_deux_racines_plt','fraction_pas_racine','fraction_x0','fraction_x1','fraction_x1_delta_denom_entier','fraction_x1_delta_num_entier','fraction_x1_sinon','fraction_x2','quitter']
 for i in range(len(filename)):
