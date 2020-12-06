a8c9532241e4875aa804e0dd349aa6317fab3439
rfoxinter <51450425+rfoxinter@users.noreply.github.com>
2020-12-06 14:00:45 +0100

    Update quitter.py

--- EquationsSecondDegre/Appli/quitter.py
+++ EquationsSecondDegre/Appli/quitter.py
@@ -2,11 +2,10 @@ import __main__
 
 def main_quit():
     __main__.plt.close()
-    __main__.root.destroy()
-
-def maj_quit():
-    __main__.root_maj.destroy()
-    filename=['main','maj']
+    __main__.root.destroy()filename=['main','maj']
     for i in range(len(filename)):
         url='https://raw.githubusercontent.com/rfoxinter/Python/master/EquationsSecondDegre/Appli/'+filename[i]+'.py'
         urllib.request.urlretrieve(url, filename[i]+'.py')
+
+def maj_quit():
+    __main__.root_maj.destroy()
