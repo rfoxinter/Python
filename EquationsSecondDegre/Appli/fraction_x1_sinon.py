8be523d72b518d82d9e1776759f4777aaef83b2f
rfoxinter <51450425+rfoxinter@users.noreply.github.com>
2020-12-07 11:34:29 +0100

    Update fraction_x1_sinon.py

--- EquationsSecondDegre/Appli/fraction_x1_sinon.py
+++ EquationsSecondDegre/Appli/fraction_x1_sinon.py
@@ -1,6 +1,7 @@
 from tkinter import *
 import __main__
 
+#Define x1 for f(x1)=0 when delta=0
 def main(a_num,a_denom,b_num,b_denom,delta_num,delta_denom):
     L4=Label(__main__.root,text='x\u2081='+str(((-b_num*a_denom*(delta_denom**0.5))-(a_denom*b_denom*(delta_num**0.5)))/(2*a_num*b_denom*(delta_denom**0.5))))
     L4.grid(column=6,row=3,sticky='w')
