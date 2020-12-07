3e4139777629dd4e9e6545212063181117a33026
rfoxinter <51450425+rfoxinter@users.noreply.github.com>
2020-12-07 11:33:24 +0100

    Update entier_deux_racines_plt.py

--- EquationsSecondDegre/Appli/entier_deux_racines_plt.py
+++ EquationsSecondDegre/Appli/entier_deux_racines_plt.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 import __main__
 
+#Plot graph when delta>0
 def main(a,b,c,alpha_num,alpha_denom,beta_num,beta_denom,delta):
     pltxmin=alpha_num/alpha_denom-10
     pltxmax=alpha_num/alpha_denom+10
