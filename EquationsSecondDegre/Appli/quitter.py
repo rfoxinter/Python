5fc4a0debe45eed33b0b22354e51df1c370f815e
rfoxinter <51450425+rfoxinter@users.noreply.github.com>
2020-12-07 11:34:22 +0100

    Update quitter.py

--- EquationsSecondDegre/Appli/quitter.py
+++ EquationsSecondDegre/Appli/quitter.py
@@ -1,24 +1,33 @@
 from tkinter import *
+import urllib.request
 import __main__
 
+def download():
+    #List of files to be downloaded
+    filename=['maj','version']
+
+    #Download the files listed
+    for i in range(len(filename)):
+        url='https://raw.githubusercontent.com/rfoxinter/Python/master/EquationsSecondDegre/Appli/'+filename[i]+'.py'
+        urllib.request.urlretrieve(url, filename[i]+'.py')
+#Close app
 def main_quit():
     def quit_quit():
         root.destroy()
     __main__.plt.close()
     __main__.root.destroy()
-    import main_version
+    download()
+    import version
     root=Tk()
+    #Check for a new version
     if main_version.version>__main__.version:
         Label(root,text='Une mise \u00E0 jour est disponible.',width=50).grid(column=0,row=0)
-        Label(root,text='Lancer main_maj.py afin de mettre \u00E0 jour  l\u2019application.',width=50).grid(column=0,row=1)
+        Label(root,text='Lancer maj.py afin de mettre \u00E0 jour  l\u2019application.',width=50).grid(column=0,row=1)
     else:
-        Label(root,text='L\u2019application est \u00E0 jour.',width=50).grid(column=0,row=0)
+        root.destroy()
     bouton_ok=Button(root,text="Ok",command=quit_quit)
     bouton_ok.grid(column=0,row=2)
     root.title("Mise \u00E0 jour")
     root.resizable(width=False,height=False)
     root.iconbitmap(r'python.ico')
     root.mainloop()
-
-def maj_quit():
-    __main__.root_maj.destroy()
