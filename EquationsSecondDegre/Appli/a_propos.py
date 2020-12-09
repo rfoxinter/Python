d2ac52cf0a9fbaf0e5aa680b2086e430812eb3c1
rfoxinter <51450425+rfoxinter@users.noreply.github.com>
2020-12-09 12:50:20 +0100

    Create a_propos.py

---
+++ EquationsSecondDegre/Appli/a_propos.py
@@ -0,0 +1,19 @@
+from tkinter import *
+import urllib.request
+import __main__
+import quitter
+
+#Close app
+def main():
+    __main__.about_root=Tk()
+    __main__.about_root.option_add('*Font', 'Arial 10')
+    Label(__main__.about_root,text='Version actuelle\u00A0:\u00A0'+str(__main__.version)).grid(column=0,row=0,sticky='w')
+    Label(__main__.about_root,text='-\u00A0Modification de la police.').grid(column=0,row=1,sticky='w')
+    Label(__main__.about_root,text='-\u00A0Modification de l\u2019icone de la fen\u00EAtre \u00E0 propos.').grid(column=0,row=3,sticky='w')
+    Label(__main__.about_root,width=50).grid(column=0,row=9)
+    bouton_fermer=Button(__main__.about_root,text="Fermer",command=quitter.about_quit)
+    bouton_fermer.grid(column=0,row=10)
+    __main__.about_root.title("\u00C0 propos")
+    __main__.about_root.resizable(width=False,height=False)
+    __main__.about_root.iconbitmap(r'a_propos.ico')
+    __main__.about_root.mainloop()
