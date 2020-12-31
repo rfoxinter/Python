d51b097206f5abf94dfc86a441b368e94adfc74b
rfoxinter <51450425+rfoxinter@users.noreply.github.com>
2020-12-31 19:11:54 +0100

    Update quitter.py

--- EquationsSecondDegre/Appli/quitter.py
+++ EquationsSecondDegre/Appli/quitter.py
@@ -27,12 +27,39 @@ def close():
         Label(root,text='Lancer maj.pyw afin de mettre \u00E0 jour  l\u2019application.',width=50).grid(column=0,row=1)
     else:
         root.destroy()
-    bouton_ok=Button(root,text="Ok",command=quit_quit)
-    bouton_ok.grid(column=0,row=2)
+    bouton_fermer=Button(root,text="Fermer",command=quit_quit)
+    bouton_fermer.grid(column=0,row=2)
     root.title("Mise \u00E0 jour")
     root.resizable(width=False,height=False)
     root.iconbitmap(r'python.ico')
     root.mainloop()
+
+def abt_maj():
+    def quit_quit():
+        quit()
+    def later():
+        maj_root.destroy()
+    download()
+    maj_root=Tk()
+    maj_root.option_add('*Font', 'Arial 10')
+    import version
+    if version.version>__main__.version:
+        url='https://raw.githubusercontent.com/rfoxinter/Python/master/EquationsSecondDegre/Appli/maj.pyw'
+        urllib.request.urlretrieve(url, 'maj.pyw')
+        Label(maj_root,text='Une mise \u00E0 jour est disponible.',width=50).grid(column=0,row=0)
+        Label(maj_root,text='Lancer maj.pyw afin de mettre \u00E0 jour  l\u2019application.',width=50).grid(column=0,row=1)
+        bouton_maj=Button(maj_root,text="Fermer pour mettre \u00E0 jour  l\u2019application",command=quit_quit)
+        bouton_maj.grid(column=0,row=2)
+        bouton_fermer=Button(maj_root,text="Ne pas mettre à jour",command=later)
+        bouton_fermer.grid(column=0,row=3)
+    else:
+        Label(maj_root,text='L\u2019application est \u00E0 jour.',width=50).grid(column=0,row=0)
+        bouton_fermer=Button(maj_root,text="Fermer",command=later)
+        bouton_fermer.grid(column=0,row=2)
+    maj_root.title("Mise \u00E0 jour")
+    maj_root.resizable(width=False,height=False)
+    maj_root.iconbitmap(r'python.ico')
+    maj_root.mainloop()
 
 #Close app
 def main_quit():
