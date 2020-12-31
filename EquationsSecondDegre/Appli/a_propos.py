ad5386a511406064c00f87fbbb2e93da66a92ec8
rfoxinter <51450425+rfoxinter@users.noreply.github.com>
2020-12-31 19:08:19 +0100

    Update a_propos.py

--- EquationsSecondDegre/Appli/a_propos.py
+++ EquationsSecondDegre/Appli/a_propos.py
@@ -8,7 +8,7 @@ def main():
     __main__.about_root=Tk()
     __main__.about_root.option_add('*Font', 'Arial 10')
     Label(__main__.about_root,text='Version actuelle\u00A0:\u00A0'+str(__main__.version)).grid(column=0,row=0,sticky='w')
-    Label(__main__.about_root,text='-\u00A0Modification de la recherche de mise \u00E0 jour.').grid(column=0,row=1,sticky='w')
+    Label(__main__.about_root,text='-\u00A0Ajout de la barre de menu.').grid(column=0,row=1,sticky='w')
     Label(__main__.about_root,width=50).grid(column=0,row=9)
     bouton_fermer=Button(__main__.about_root,text="Fermer",command=quitter.about_quit)
     bouton_fermer.grid(column=0,row=10)
