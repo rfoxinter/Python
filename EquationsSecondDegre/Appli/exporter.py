38417141a4abefee9ddc51c22a7a5496eff172a9
rfoxinter <51450425+rfoxinter@users.noreply.github.com>
2021-01-07 18:33:56 +0100

    Update exporter.py

--- EquationsSecondDegre/Appli/exporter.py
+++ EquationsSecondDegre/Appli/exporter.py
@@ -3,16 +3,19 @@ import pathlib
 import __main__
 
 def main(event=None):
-    file=asksaveasfile(mode='w',title='Enregistrer l\u2019\u00E9quation',filetypes=[('Fichier texte','*.txt'),('Rich Text Format','*.rtf')],defaultextension='*.txt')
+    file=asksaveasfile(title='Enregistrer l\u2019\u00E9quation',filetypes=[('Fichier texte','.txt .text'),('Rich Text Format','.rtf'),('Hypertext Markup Language','.html .htm')],defaultextension='.txt')
     if file is None:
         return
     if __main__.latest=='entier':
         __main__.rep[0]='('+str(__main__.entier_val[0])+')x\u00B2+('+str(__main__.entier_val[1])+')x+('+str(__main__.entier_val[2])+')'
     elif __main__.latest=='fraction':
-        __main__.rep[0]='('+str(__main__.entier_val[0])+'/'+str(__main__.entier_val[1])+')x\u00B2+('+str(__main__.entier_val[2])+'/'+str(__main__.entier_val[3])+')x+('+str(__main__.entier_val[4])+'/'+str(__main__.entier_val[5])+')'
+        __main__.rep[0]='('+str(__main__.fraction_val[0])+'/'+str(__main__.fraction_val[1])+')x\u00B2+('+str(__main__.fraction_val[2])+'/'+str(__main__.fraction_val[3])+')x+('+str(__main__.fraction_val[4])+'/'+str(__main__.fraction_val[5])+')'
     if  pathlib.Path(file.name).suffix=='.rtf':
         from extensions import rtf
         rtf.main(file.name)
+    elif pathlib.Path(file.name).suffix=='.html' or pathlib.Path(file.name).suffix=='.htm':
+        from extensions import html
+        html.main(file.name)
     else:
         from extensions import txt
         txt.main(file.name)
