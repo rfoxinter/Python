8768718b62d809ca981c0c9f0731a2f9353246b3
rfoxinter <51450425+rfoxinter@users.noreply.github.com>
2021-01-07 19:38:59 +0100

    Create tex.py

---
+++ EquationsSecondDegre/Appli/extensions/tex.py
@@ -0,0 +1,26 @@
+import __main__
+
+def main(path):
+    __main__.rep[0]=__main__.rep[0].replace("\u00B2","\u005E2")
+    __main__.rep[1]=' '
+    __main__.rep[2]=__main__.rep[2].replace("\u0394","\u005CDelta")
+    __main__.rep[3]=__main__.rep[3].replace("\u03B1","\u005Calpha")
+    __main__.rep[4]=__main__.rep[4].replace("\u03B2","\u005Cbeta")
+    __main__.rep[5]=__main__.rep[5].replace(" ","\u005C:")
+    __main__.rep[5]=__main__.rep[5].replace("\u00E9","\u005Cacute e")
+    __main__.rep[5]=__main__.rep[5].replace("\u2080","_0")
+    __main__.rep[5]=__main__.rep[5].replace("\u2081","_1")
+    __main__.rep[6]=__main__.rep[6].replace("\u2082","_2")
+    if __main__.rep[6]=='':
+        __main__.rep[6]=' '
+    file_write=open(path,'w',encoding="utf-8")
+    file_write.writelines('\u005Cdocumentclass\u005Ba4paper\u005D\u007Barticle\u007D\n\u005Cusepackage\u005Butf8\u005D\u007Binputenc\u007D\n\u005Csetlength\u007B\u005Cparindent\u007D\u007B0cm\u007D\n\u005Cbegin\u007Bdocument\u007D\n')
+    file_write.close()
+    for i in range(len(__main__.rep)):
+        file_write=open(path,'a',encoding="utf-8")
+        file_write.writelines('$'+__main__.rep[i]+'$\u005C\u005C\n')
+        file_write.close()
+    file_write=open(path,'a',encoding="utf-8")
+    file_write.writelines('\u005Cend\u007Bdocument\u007D')
+    file_write.close()
+    __main__.rep[1]=''
