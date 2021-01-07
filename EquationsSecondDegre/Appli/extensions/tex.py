564b5401cba0c1a41867c357e04b7583320958fc
rfoxinter <51450425+rfoxinter@users.noreply.github.com>
2021-01-07 19:51:22 +0100

    Update tex.py

--- EquationsSecondDegre/Appli/extensions/tex.py
+++ EquationsSecondDegre/Appli/extensions/tex.py
@@ -1,6 +1,12 @@
 import __main__
 
 def main(path):
+    rep_0=__main__.rep[0]
+    rep_2=__main__.rep[2]
+    rep_3=__main__.rep[3]
+    rep_4=__main__.rep[4]
+    rep_5=__main__.rep[5]
+    rep_6=__main__.rep[6]
     __main__.rep[0]=__main__.rep[0].replace("\u00B2","\u005E2")
     __main__.rep[1]=' '
     __main__.rep[2]=__main__.rep[2].replace("\u0394","\u005CDelta")
@@ -23,4 +29,10 @@ def main(path):
     file_write=open(path,'a',encoding="utf-8")
     file_write.writelines('\u005Cend\u007Bdocument\u007D')
     file_write.close()
+    __main__.rep[0]=rep_0
     __main__.rep[1]=''
+    __main__.rep[2]=rep_2
+    __main__.rep[3]=rep_3
+    __main__.rep[4]=rep_4
+    __main__.rep[5]=rep_5
+    __main__.rep[6]=rep_6
