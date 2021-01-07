f0d261c257f2d4152f1b69bf448ebeb79ba5d764
rfoxinter <51450425+rfoxinter@users.noreply.github.com>
2021-01-07 19:51:20 +0100

    Update rtf.py

--- EquationsSecondDegre/Appli/extensions/rtf.py
+++ EquationsSecondDegre/Appli/extensions/rtf.py
@@ -1,6 +1,12 @@
 import __main__
 
 def main(path):
+    rep_0=__main__.rep[0]
+    rep_2=__main__.rep[2]
+    rep_3=__main__.rep[3]
+    rep_4=__main__.rep[4]
+    rep_5=__main__.rep[5]
+    rep_6=__main__.rep[6]
     __main__.rep[0]=__main__.rep[0].replace("\u00B2","\u005C'b2")
     __main__.rep[2]=__main__.rep[2].replace("\u0394","\u005C'c4")
     __main__.rep[3]=__main__.rep[3].replace("\u03B1","\u005C'e1")
@@ -20,3 +26,9 @@ def main(path):
     file_write=open(path,'a',encoding="utf-8")
     file_write.writelines('}')
     file_write.close()
+    __main__.rep[0]=rep_0
+    __main__.rep[2]=rep_2
+    __main__.rep[3]=rep_3
+    __main__.rep[4]=rep_4
+    __main__.rep[5]=rep_5
+    __main__.rep[6]=rep_6
