caf07ff42eb35b8f4c017f6ddabd7845a5c07b41
rfoxinter <51450425+rfoxinter@users.noreply.github.com>
2021-01-09 21:34:40 +0100

    Update sauvegarder.py

--- EquationsSecondDegre/Appli/sauvegarder.py
+++ EquationsSecondDegre/Appli/sauvegarder.py
@@ -1,14 +1,14 @@
 from tkinter.filedialog import asksaveasfile
-import __main__
+import prgm
 
 def main(event=None):
     file=asksaveasfile(mode='w',title='Enregistrer l\u2019\u00E9quation',filetypes=[('Fichier \u00E9quation','.eq')],defaultextension='.eq')
     if file is None:
         return
-    if __main__.latest=='entier':
-        for l in range(len(__main__.entier_val)):
-            file.writelines(str(__main__.entier_val[l])+'\n')
-    elif __main__.latest=='fraction':
-        for l in range(len(__main__.fraction_val)):
-            file.writelines(str(__main__.fraction_val[l])+'\n')
+    if prgm.latest=='entier':
+        for l in range(len(prgm.entier_val)):
+            file.writelines(str(prgm.entier_val[l])+'\n')
+    elif prgm.latest=='fraction':
+        for l in range(len(prgm.fraction_val)):
+            file.writelines(str(prgm.fraction_val[l])+'\n')
     file.close()
