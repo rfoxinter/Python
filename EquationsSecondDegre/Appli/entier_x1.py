e29d15b2a85ec1fc834e06a75aa32753ab5bedf5
rfoxinter <51450425+rfoxinter@users.noreply.github.com>
2021-01-09 21:36:15 +0100

    Update entier_x1.py

--- EquationsSecondDegre/Appli/entier_x1.py
+++ EquationsSecondDegre/Appli/entier_x1.py
@@ -1,5 +1,5 @@
 from tkinter import Label
-import __main__
+import prgm
 
 #Define x1 for f(x1)=0 when delta>0
 def main(a,b,delta):
@@ -8,9 +8,9 @@ def main(a,b,delta):
         x1_denom=2*a
         if x1_num%x1_denom==0:
             x1=x1_num//x1_denom
-            L4=Label(__main__.root,text='x\u2081='+str(int(x1)))
+            L4=Label(prgm.root,text='x\u2081='+str(int(x1)))
             L4.grid(column=6,row=3,sticky='w')
-            __main__.rep[5]=L4.cget('text')
+            prgm.rep[5]=L4.cget('text')
         else:
             div_x1=2
             while div_x1<=abs(x1_num) and div_x1<=abs(x1_denom):
@@ -20,13 +20,13 @@ def main(a,b,delta):
                 else:
                     div_x1=div_x1+1
             if x1_num<0 and x1_denom<0:
-                L4=Label(__main__.root,text='x\u2081=('+str(int(-x1_num))+')/('+str(int(-x1_denom))+')='+str(x1_num/x1_denom).replace('.',','))
+                L4=Label(prgm.root,text='x\u2081=('+str(int(-x1_num))+')/('+str(int(-x1_denom))+')='+str(x1_num/x1_denom).replace('.',','))
                 L4.grid(column=6,row=3,sticky='w')
-                __main__.rep[5]=L4.cget('text')
+                prgm.rep[5]=L4.cget('text')
             else:
-                L4=Label(__main__.root,text='x\u2081=('+str(int(x1_num))+')/('+str(int(x1_denom))+')='+str(x1_num/x1_denom).replace('.',','))
+                L4=Label(prgm.root,text='x\u2081=('+str(int(x1_num))+')/('+str(int(x1_denom))+')='+str(x1_num/x1_denom).replace('.',','))
                 L4.grid(column=6,row=3,sticky='w')
-                __main__.rep[5]=L4.cget('text')
+                prgm.rep[5]=L4.cget('text')
     else:
         x1_num_int=-b
         x1_num_rt=delta
@@ -45,33 +45,33 @@ def main(a,b,delta):
                 div_x1=div_x1+1
         if x1_num_int==0:
             if abs(x1_denom)==1:
-                L4=Label(__main__.root,text='x\u2081=\u221A('+str(x1_num_rt)+')='+str(x1_num_rt**0.5).replace('.',','))
+                L4=Label(prgm.root,text='x\u2081=\u221A('+str(x1_num_rt)+')='+str(x1_num_rt**0.5).replace('.',','))
                 L4.grid(column=6,row=3,sticky='w')
-                __main__.rep[5]=L4.cget('text')
+                prgm.rep[5]=L4.cget('text')
             else:
                 if x1_denom<0:
-                    L4=Label(__main__.root,text='x\u2081=\u221A('+str(x1_num_rt)+')/('+str(int(-x1_denom))+')='+str((x1_num_rt**0.5)/-x1_denom).replace('.',','))
+                    L4=Label(prgm.root,text='x\u2081=\u221A('+str(x1_num_rt)+')/('+str(int(-x1_denom))+')='+str((x1_num_rt**0.5)/-x1_denom).replace('.',','))
                     L4.grid(column=6,row=3,sticky='w')
-                    __main__.rep[5]=L4.cget('text')
+                    prgm.rep[5]=L4.cget('text')
                 else:
-                    L4=Label(__main__.root,text='x\u2081=-\u221A('+str(x1_num_rt)+')/('+str(int(x1_denom))+')='+str(-(x1_num_rt**0.5)/x1_denom).replace('.',','))
+                    L4=Label(prgm.root,text='x\u2081=-\u221A('+str(x1_num_rt)+')/('+str(int(x1_denom))+')='+str(-(x1_num_rt**0.5)/x1_denom).replace('.',','))
                     L4.grid(column=6,row=3,sticky='w')
-                    __main__.rep[5]=L4.cget('text')
+                    prgm.rep[5]=L4.cget('text')
         elif x1_num_int<0 and x1_denom<0:
             if abs(x1_denom)==1:
-                L4=Label(__main__.root,text='x\u2081='+str(int(-x1_num_int))+'+\u221A('+str(x1_num_rt)+')='+str(x1_num_int+x1_num_rt**0.5).replace('.',','))
+                L4=Label(prgm.root,text='x\u2081='+str(int(-x1_num_int))+'+\u221A('+str(x1_num_rt)+')='+str(x1_num_int+x1_num_rt**0.5).replace('.',','))
                 L4.grid(column=6,row=3,sticky='w')
-                __main__.rep[5]=L4.cget('text')
+                prgm.rep[5]=L4.cget('text')
             else:
-                L4=Label(__main__.root,text='x\u2081=('+str(int(-x1_num_int))+'+\u221A('+str(x1_num_rt)+'))/('+str(int(-x1_denom))+')='+str((x1_num_int+x1_num_rt**0.5)/x1_denom).replace('.',','))
+                L4=Label(prgm.root,text='x\u2081=('+str(int(-x1_num_int))+'+\u221A('+str(x1_num_rt)+'))/('+str(int(-x1_denom))+')='+str((x1_num_int+x1_num_rt**0.5)/x1_denom).replace('.',','))
                 L4.grid(column=6,row=3,sticky='w')
-                __main__.rep[5]=L4.cget('text')
+                prgm.rep[5]=L4.cget('text')
         else:
             if abs(x1_denom)==1:
-                L4=Label(__main__.root,text='x\u2081='+str(int(x1_num_int))+'-\u221A('+str(x1_num_rt)+')='+str(x1_num_int-x1_num_rt**0.5).replace('.',','))
+                L4=Label(prgm.root,text='x\u2081='+str(int(x1_num_int))+'-\u221A('+str(x1_num_rt)+')='+str(x1_num_int-x1_num_rt**0.5).replace('.',','))
                 L4.grid(column=6,row=3,sticky='w')
-                __main__.rep[5]=L4.cget('text')
+                prgm.rep[5]=L4.cget('text')
             else:
-                L4=Label(__main__.root,text='x\u2081=('+str(int(x1_num_int))+'-\u221A('+str(x1_num_rt)+'))/('+str(int(x1_denom))+')='+str((x1_num_int-x1_num_rt**0.5)/x1_denom).replace('.',','))
+                L4=Label(prgm.root,text='x\u2081=('+str(int(x1_num_int))+'-\u221A('+str(x1_num_rt)+'))/('+str(int(x1_denom))+')='+str((x1_num_int-x1_num_rt**0.5)/x1_denom).replace('.',','))
                 L4.grid(column=6,row=3,sticky='w')
-                __main__.rep[5]=L4.cget('text')
+                prgm.rep[5]=L4.cget('text')
