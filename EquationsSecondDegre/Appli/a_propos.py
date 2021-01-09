82e46e0001dbdf1256f265545f97069305d2b50d
rfoxinter <51450425+rfoxinter@users.noreply.github.com>
2021-01-09 21:36:34 +0100

    Update a_propos.py

--- EquationsSecondDegre/Appli/a_propos.py
+++ EquationsSecondDegre/Appli/a_propos.py
@@ -1,18 +1,18 @@
 from tkinter import Tk,Label,Button
 import urllib.request
-import __main__
+import prgm
 import quitter
 
 #Close app
 def main():
-    __main__.about_root=Tk()
-    __main__.about_root.option_add('*Font', 'Arial 10')
-    Label(__main__.about_root,text='Version actuelle\u00A0:\u00A0'+str(__main__.version)).grid(column=0,row=0,sticky='w')
-    Label(__main__.about_root,text='-\u00A0Am\u00E9lioration de l\u2019export d\u2019\u00E9quations au format LaTeX.').grid(column=0,row=1,sticky='w')
-    Label(__main__.about_root,width=50).grid(column=0,row=9)
-    bouton_fermer=Button(__main__.about_root,text="Fermer",command=quitter.about_quit)
+    prgm.about_root=Tk()
+    prgm.about_root.option_add('*Font', 'Arial 10')
+    Label(prgm.about_root,text='Version actuelle\u00A0:\u00A0'+str(prgm.version)).grid(column=0,row=0,sticky='w')
+    Label(prgm.about_root,text='-\u00A0Am\u00E9lioration de l\u2019export d\u2019\u00E9quations au format LaTeX.').grid(column=0,row=1,sticky='w')
+    Label(prgm.about_root,width=50).grid(column=0,row=9)
+    bouton_fermer=Button(prgm.about_root,text="Fermer",command=quitter.about_quit)
     bouton_fermer.grid(column=0,row=10)
-    __main__.about_root.title("\u00C0 propos")
-    __main__.about_root.resizable(width=False,height=False)
-    __main__.about_root.iconbitmap(r'a_propos.ico')
-    __main__.about_root.mainloop()
+    prgm.about_root.title("\u00C0 propos")
+    prgm.about_root.resizable(width=False,height=False)
+    prgm.about_root.iconbitmap(r'a_propos.ico')
+    prgm.about_root.mainloop()
