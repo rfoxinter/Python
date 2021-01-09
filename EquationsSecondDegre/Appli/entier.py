68d1b9f706673c450c5f375c35a8c367474395e9
rfoxinter <51450425+rfoxinter@users.noreply.github.com>
2021-01-09 21:36:31 +0100

    Update entier.py

--- EquationsSecondDegre/Appli/entier.py
+++ EquationsSecondDegre/Appli/entier.py
@@ -1,5 +1,5 @@
 from tkinter import Label
-import __main__
+import prgm
 import entier_pas_racine
 import entier_x0
 import entier_x1
@@ -9,55 +9,55 @@ import preferences
 
 def main_ent():
     if preferences.afficher_graphs==1:
-        __main__.plt.close()
-    for label in __main__.root.grid_slaves():
+        prgm.plt.close()
+    for label in prgm.root.grid_slaves():
         if int(label.grid_info()['column'])==6:
             label.destroy()
-    Col7=Label(__main__.root,width=100)
+    Col7=Label(prgm.root,width=100)
     Col7.grid(column=6,row=5)
-    if __main__.ValeurA.get()!=0:
-        __main__.latest='entier'
-        sec_ent(__main__.ValeurA.get(),__main__.ValeurB.get(),__main__.ValeurC.get())
+    if prgm.ValeurA.get()!=0:
+        prgm.latest='entier'
+        sec_ent(prgm.ValeurA.get(),prgm.ValeurB.get(),prgm.ValeurC.get())
     else:
-        L1=Label(__main__.root,text='Erreur\u00A0:\u00A0a\u22600')
+        L1=Label(prgm.root,text='Erreur\u00A0:\u00A0a\u22600')
         L1.grid(column=6,row=0,sticky='w')
 
 def open_ent(a,b,c):
     if preferences.afficher_graphs==1:
-        __main__.plt.close()
-    for label in __main__.root.grid_slaves():
+        prgm.plt.close()
+    for label in prgm.root.grid_slaves():
         if int(label.grid_info()['column'])==6:
             label.destroy()
-    Col7=Label(__main__.root,width=100)
+    Col7=Label(prgm.root,width=100)
     Col7.grid(column=6,row=5)
-    if __main__.ValeurA.get()!=0:
-        __main__.latest='entier'
-        __main__.entier_val[0]=a
-        __main__.entier_val[1]=b
-        __main__.entier_val[2]=c
+    if prgm.ValeurA.get()!=0:
+        prgm.latest='entier'
+        prgm.entier_val[0]=a
+        prgm.entier_val[1]=b
+        prgm.entier_val[2]=c
         sec_ent(a,b,c)
     else:
-        L1=Label(__main__.root,text='Erreur\u00A0:\u00A0a\u22600')
+        L1=Label(prgm.root,text='Erreur\u00A0:\u00A0a\u22600')
         L1.grid(column=6,row=0,sticky='w')
 
 def sec_ent(a,b,c):
-    __main__.rep[6]=''
-    __main__.entier_val[0]=a
-    __main__.entier_val[1]=b
-    __main__.entier_val[2]=c
+    prgm.rep[6]=''
+    prgm.entier_val[0]=a
+    prgm.entier_val[1]=b
+    prgm.entier_val[2]=c
     delta=b**2-4*a*c+(int(a/a)-1)
     alpha_num=-b
     alpha_denom=2*a
     beta_num=-delta
     beta_denom=4*a
-    L1=Label(__main__.root,text='\u0394='+str(delta))
+    L1=Label(prgm.root,text='\u0394='+str(delta))
     L1.grid(column=6,row=0,sticky='w')
-    __main__.rep[2]=L1.cget('text')
+    prgm.rep[2]=L1.cget('text')
     if alpha_num%alpha_denom==0:
         alpha=alpha_num//alpha_denom
-        L2=Label(__main__.root,text='\u03B1='+str(alpha))
+        L2=Label(prgm.root,text='\u03B1='+str(alpha))
         L2.grid(column=6,row=1,sticky='w')
-        __main__.rep[3]=L2.cget('text')
+        prgm.rep[3]=L2.cget('text')
     else:
         div_alpha=2
         while div_alpha<=abs(alpha_num) and div_alpha<=abs(alpha_denom):
@@ -67,19 +67,19 @@ def sec_ent(a,b,c):
             else:
                 div_alpha=div_alpha+1
         if alpha_denom<0:
-            L2=Label(__main__.root,text='\u03B1=('+str(int(-alpha_num))+')/('+str(int(-alpha_denom))+')='+str(alpha_num/alpha_denom).replace('.',','))
+            L2=Label(prgm.root,text='\u03B1=('+str(int(-alpha_num))+')/('+str(int(-alpha_denom))+')='+str(alpha_num/alpha_denom).replace('.',','))
             L2.grid(column=6,row=1,sticky='w')
-            __main__.rep[3]=L2.cget('text')
-            __main__.rep[3]=L2.cget('text')
+            prgm.rep[3]=L2.cget('text')
+            prgm.rep[3]=L2.cget('text')
         else:
-            L2=Label(__main__.root,text='\u03B1=('+str(int(alpha_num))+')/('+str(int(alpha_denom))+')='+str(alpha_num/alpha_denom).replace('.',','))
+            L2=Label(prgm.root,text='\u03B1=('+str(int(alpha_num))+')/('+str(int(alpha_denom))+')='+str(alpha_num/alpha_denom).replace('.',','))
             L2.grid(column=6,row=1,sticky='w')
-            __main__.rep[3]=L2.cget('text')
+            prgm.rep[3]=L2.cget('text')
     if beta_num%beta_denom==0:
         beta=beta_num//beta_denom
-        L3=Label(__main__.root,text='\u03B2='+str(beta))
+        L3=Label(prgm.root,text='\u03B2='+str(beta))
         L3.grid(column=6,row=2,sticky='w')
-        __main__.rep[4]=L3.cget('text')
+        prgm.rep[4]=L3.cget('text')
     else:
         div_beta=2
         while div_beta<=abs(beta_num) and div_beta<=abs(beta_denom):
@@ -89,13 +89,13 @@ def sec_ent(a,b,c):
             else:
                 div_beta=div_beta+1
         if beta_denom<0:
-            L3=Label(__main__.root,text='\u03B2=('+str(int(-beta_num))+')/('+str(int(-beta_denom))+')='+str(beta_num/beta_denom).replace('.',','))
+            L3=Label(prgm.root,text='\u03B2=('+str(int(-beta_num))+')/('+str(int(-beta_denom))+')='+str(beta_num/beta_denom).replace('.',','))
             L3.grid(column=6,row=2,sticky='w')
-            __main__.rep[4]=L3.cget('text')
+            prgm.rep[4]=L3.cget('text')
         else:
-            L3=Label(__main__.root,text='\u03B2=('+str(int(beta_num))+')/('+str(int(beta_denom))+')='+str(beta_num/beta_denom).replace('.',','))
+            L3=Label(prgm.root,text='\u03B2=('+str(int(beta_num))+')/('+str(int(beta_denom))+')='+str(beta_num/beta_denom).replace('.',','))
             L3.grid(column=6,row=2,sticky='w')
-            __main__.rep[4]=L3.cget('text')
+            prgm.rep[4]=L3.cget('text')
     if delta<0:
         entier_pas_racine.main(a,b,c,alpha_num,alpha_denom,beta_num,beta_denom)
     elif delta==0:
