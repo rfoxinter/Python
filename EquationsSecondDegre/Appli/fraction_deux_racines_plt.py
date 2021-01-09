be087be8fbaac20f72cc01aeaaa0cb973cdd3452
rfoxinter <51450425+rfoxinter@users.noreply.github.com>
2021-01-09 21:36:03 +0100

    Update fraction_deux_racines_plt.py

--- EquationsSecondDegre/Appli/fraction_deux_racines_plt.py
+++ EquationsSecondDegre/Appli/fraction_deux_racines_plt.py
@@ -1,5 +1,5 @@
 import preferences
-import __main__
+import prgm
 
 #Plot graph when delta>0
 def main(a_num,a_denom,b_num,b_denom,c_num,c_denom,alpha_num,alpha_denom,beta_num,beta_denom,delta_num,delta_denom):
@@ -21,12 +21,12 @@ def main(a_num,a_denom,b_num,b_denom,c_num,c_denom,alpha_num,alpha_denom,beta_nu
             ymax=(a_num/a_denom)*(pltxmin)**2+(b_num/b_denom)*(pltxmin)+(c_num/c_denom)
     x=np.linspace(pltxmin,pltxmax,1000)
     y=(a_num/a_denom)*x**2+(b_num/b_denom)*x+(c_num/c_denom)
-    __main__.plt.plot(x,y,c='blue')
-    __main__.plt.scatter(alpha_num/alpha_denom,beta_num/beta_denom,c='red',marker='x',label='(\u03B1;\u03B2)')
-    __main__.plt.scatter((-(b_num/b_denom)-((delta_num/delta_denom)**0.5))/(2*(a_num/a_denom)),0,c='green',marker='x',label='x\u2081=(((-b-\u221A(\u0394))/2a);0)')
-    __main__.plt.scatter((-(b_num/b_denom)+((delta_num/delta_denom)**0.5))/(2*(a_num/a_denom)),0,c='violet',marker='x',label='x\u2082=(((-b+\u221A(\u0394))/2a);0)')
-    __main__.plt.xlabel('x')
-    __main__.plt.ylabel('y=f(x)')
+    prgm.plt.plot(x,y,c='blue')
+    prgm.plt.scatter(alpha_num/alpha_denom,beta_num/beta_denom,c='red',marker='x',label='(\u03B1;\u03B2)')
+    prgm.plt.scatter((-(b_num/b_denom)-((delta_num/delta_denom)**0.5))/(2*(a_num/a_denom)),0,c='green',marker='x',label='x\u2081=(((-b-\u221A(\u0394))/2a);0)')
+    prgm.plt.scatter((-(b_num/b_denom)+((delta_num/delta_denom)**0.5))/(2*(a_num/a_denom)),0,c='violet',marker='x',label='x\u2082=(((-b+\u221A(\u0394))/2a);0)')
+    prgm.plt.xlabel('x')
+    prgm.plt.ylabel('y=f(x)')
     pltymin=0
     pltymax=0
     if beta_num/beta_denom-ymax/100<ymax:
@@ -35,9 +35,9 @@ def main(a_num,a_denom,b_num,b_denom,c_num,c_denom,alpha_num,alpha_denom,beta_nu
     else:
         pltymin=ymax
         pltymax=beta_num/beta_denom-ymax/100
-    __main__.plt.axis([pltxmin,pltxmax,pltymin,pltymax])
-    __main__.plt.axhline(y=0,c='black')
-    __main__.plt.axvline(x=0,c='black')
-    __main__.plt.legend()
-    __main__.plt.title('f(x)=('+str(a_num)+')/('+str(a_denom)+')x\u00B2+('+str(b_num)+')/('+str(b_denom)+')x+('+str(c_num)+')/('+str(c_denom)+')')
-    __main__.plt.show()
+    prgm.plt.axis([pltxmin,pltxmax,pltymin,pltymax])
+    prgm.plt.axhline(y=0,c='black')
+    prgm.plt.axvline(x=0,c='black')
+    prgm.plt.legend()
+    prgm.plt.title('f(x)=('+str(a_num)+')/('+str(a_denom)+')x\u00B2+('+str(b_num)+')/('+str(b_denom)+')x+('+str(c_num)+')/('+str(c_denom)+')')
+    prgm.plt.show()
