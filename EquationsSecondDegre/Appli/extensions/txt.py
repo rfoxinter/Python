090415b457099a8a5fa54acc0c18a1a913b36614
rfoxinter <51450425+rfoxinter@users.noreply.github.com>
2021-01-10 14:10:27 +0100

    Update txt.py

--- EquationsSecondDegre/Appli/extensions/txt.py
+++ EquationsSecondDegre/Appli/extensions/txt.py
@@ -1,10 +1,10 @@
-import __main__
+import prgm
 
 def main(path):
     file_write=open(path,'w',encoding="utf-8")
     file_write.writelines('')
     file_write.close()
-    for i in range(len(__main__.rep)):
+    for i in range(len(prgm.rep)):
         file_write=open(path,'a',encoding="utf-8")
-        file_write.writelines(__main__.rep[i]+'\n')
+        file_write.writelines(prgm.rep[i]+'\n')
         file_write.close()
