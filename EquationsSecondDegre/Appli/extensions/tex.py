3d825da37213abc4c854c9b5b11cec2ed53b5297
rfoxinter <51450425+rfoxinter@users.noreply.github.com>
2021-01-10 14:10:24 +0100

    Update tex.py

--- EquationsSecondDegre/Appli/extensions/tex.py
+++ EquationsSecondDegre/Appli/extensions/tex.py
@@ -1,47 +1,47 @@
-import __main__
+import prgm
 
 def main(path):
-    rep_0=__main__.rep[0]
-    rep_2=__main__.rep[2]
-    rep_3=__main__.rep[3]
-    rep_4=__main__.rep[4]
-    rep_5=__main__.rep[5]
-    rep_6=__main__.rep[6]
-    __main__.rep[0]=__main__.rep[0].replace("\u00B2","\u005E2")
-    __main__.rep[1]=' '
-    __main__.rep[2]=__main__.rep[2].replace("\u0394","\u005CDelta")
-    __main__.rep[3]=__main__.rep[3].replace("\u03B1","\u005Calpha")
-    __main__.rep[4]=__main__.rep[4].replace("\u03B2","\u005Cbeta")
-    __main__.rep[5]=__main__.rep[5].replace(" ","\u005C:")
-    __main__.rep[5]=__main__.rep[5].replace("\u00E9","\u005Cacute e")
-    __main__.rep[5]=__main__.rep[5].replace("\u2080","_0")
-    __main__.rep[5]=__main__.rep[5].replace("\u2081","_1")
-    __main__.rep[6]=__main__.rep[6].replace("\u2082","_2")
+    rep_0=prgm.rep[0]
+    rep_2=prgm.rep[2]
+    rep_3=prgm.rep[3]
+    rep_4=prgm.rep[4]
+    rep_5=prgm.rep[5]
+    rep_6=prgm.rep[6]
+    prgm.rep[0]=prgm.rep[0].replace("\u00B2","\u005E2")
+    prgm.rep[1]=' '
+    prgm.rep[2]=prgm.rep[2].replace("\u0394","\u005CDelta")
+    prgm.rep[3]=prgm.rep[3].replace("\u03B1","\u005Calpha")
+    prgm.rep[4]=prgm.rep[4].replace("\u03B2","\u005Cbeta")
+    prgm.rep[5]=prgm.rep[5].replace(" ","\u005C:")
+    prgm.rep[5]=prgm.rep[5].replace("\u00E9","\u005Cacute e")
+    prgm.rep[5]=prgm.rep[5].replace("\u2080","_0")
+    prgm.rep[5]=prgm.rep[5].replace("\u2081","_1")
+    prgm.rep[6]=prgm.rep[6].replace("\u2082","_2")
     for k in range(2):
-        for i in range(len(__main__.rep[k+5])):
-            if __main__.rep[k+5][i]=="\u221A" :
-                ifin=__main__.rep[k+5].find(")",i)
-                __main__.rep[k+5]=__main__.rep[k+5].replace(__main__.rep[k+5][i:ifin+1],"\u005Csqrt{"+__main__.rep[k+5][i+2:ifin]+"}")
+        for i in range(len(prgm.rep[k+5])):
+            if prgm.rep[k+5][i]=="\u221A" :
+                ifin=prgm.rep[k+5].find(")",i)
+                prgm.rep[k+5]=prgm.rep[k+5].replace(prgm.rep[k+5][i:ifin+1],"\u005Csqrt{"+prgm.rep[k+5][i+2:ifin]+"}")
     for k in range(7):
-        __main__.rep[k]=__main__.rep[k].replace(")/(","}{")
-        __main__.rep[k]=__main__.rep[k].replace(")","}")
-        __main__.rep[k]=__main__.rep[k].replace("(","\u005Cfrac{")
-    if __main__.rep[6]=='':
-        __main__.rep[6]=' '
+        prgm.rep[k]=prgm.rep[k].replace(")/(","}{")
+        prgm.rep[k]=prgm.rep[k].replace(")","}")
+        prgm.rep[k]=prgm.rep[k].replace("(","\u005Cfrac{")
+    if prgm.rep[6]=='':
+        prgm.rep[6]=' '
     file_write=open(path,'w',encoding="utf-8")
     file_write.writelines('\u005Cdocumentclass\u005Ba4paper\u005D\u007Barticle\u007D\n\u005Cusepackage\u005Butf8\u005D\u007Binputenc\u007D\n\u005Csetlength\u007B\u005Cparindent\u007D\u007B0cm\u007D\n\u005Cbegin\u007Bdocument\u007D\n')
     file_write.close()
-    for i in range(len(__main__.rep)):
+    for i in range(len(prgm.rep)):
         file_write=open(path,'a',encoding="utf-8")
-        file_write.writelines('$'+__main__.rep[i]+'$\u005C\u005C\n')
+        file_write.writelines('$'+prgm.rep[i]+'$\u005C\u005C\n')
         file_write.close()
     file_write=open(path,'a',encoding="utf-8")
     file_write.writelines('\u005Cend\u007Bdocument\u007D')
     file_write.close()
-    __main__.rep[0]=rep_0
-    __main__.rep[1]=''
-    __main__.rep[2]=rep_2
-    __main__.rep[3]=rep_3
-    __main__.rep[4]=rep_4
-    __main__.rep[5]=rep_5
-    __main__.rep[6]=rep_6
+    prgm.rep[0]=rep_0
+    prgm.rep[1]=''
+    prgm.rep[2]=rep_2
+    prgm.rep[3]=rep_3
+    prgm.rep[4]=rep_4
+    prgm.rep[5]=rep_5
+    prgm.rep[6]=rep_6
