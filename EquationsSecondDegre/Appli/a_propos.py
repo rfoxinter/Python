ff84c74cfd3edbad5716b4e878b27da0fa84de0e
rfoxinter <51450425+rfoxinter@users.noreply.github.com>
2021-01-10 14:10:14 +0100

    Update a_propos.py

--- EquationsSecondDegre/Appli/a_propos.py
+++ EquationsSecondDegre/Appli/a_propos.py
@@ -8,7 +8,7 @@ def main():
     prgm.about_root=Tk()
     prgm.about_root.option_add('*Font', 'Arial 10')
     Label(prgm.about_root,text='Version actuelle\u00A0:\u00A0'+str(prgm.version)).grid(column=0,row=0,sticky='w')
-    Label(prgm.about_root,text='-\u00A0Am\u00E9lioration de l\u2019export d\u2019\u00E9quations au format LaTeX.').grid(column=0,row=1,sticky='w')
+    Label(prgm.about_root,text='-\u00A0Am\u00E9lioration de l\u2019export d\u2019\u00E9quations au format HTML.').grid(column=0,row=1,sticky='w')
     Label(prgm.about_root,width=50).grid(column=0,row=9)
     bouton_fermer=Button(prgm.about_root,text="Fermer",command=quitter.about_quit)
     bouton_fermer.grid(column=0,row=10)
