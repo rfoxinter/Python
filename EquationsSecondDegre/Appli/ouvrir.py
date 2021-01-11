6911289aa56a4e45ef4719c5e3270f6025df3d62
rfoxinter <51450425+rfoxinter@users.noreply.github.com>
2021-01-11 21:44:44 +0100

    Update ouvrir.py

--- EquationsSecondDegre/Appli/ouvrir.py
+++ EquationsSecondDegre/Appli/ouvrir.py
@@ -6,33 +6,22 @@ import fraction
 def main(event=None):
     file=askopenfilename(title='S\u00E9lectionner un fichier à ouvrir',filetypes=[('Fichier \u00E9quation', '.eq')],defaultextension='.eq')
     lire=open(file,'r')
-    lignes=len(lire.readlines())
-    with open(file) as f:
-        data = f.readlines()[0]
-    if lignes<6:
-        with open(file) as f:
-            a=f.readlines()[0]
-        with open(file) as f:
-            b=f.readlines()[1]
-        with open(file) as f:
-            c=f.readlines()[2]
+    lignes=lire.readlines()
+    if len(lignes)<6:
+        a=int(lignes[0])
+        b=int(lignes[1])
+        c=int(lignes[2])
         prgm.ValeurA.set(a)
         prgm.ValeurB.set(b)
         prgm.ValeurC.set(b)
-        entier.open_ent(int(a),int(b),int(c))
+        entier.open_ent(a,b,c)
     else:
-        with open(file) as f:
-            a_num=f.readlines()[0]
-        with open(file) as f:
-            a_denom=f.readlines()[1]
-        with open(file) as f:
-            b_num=f.readlines()[2]
-        with open(file) as f:
-            b_denom=f.readlines()[3]
-        with open(file) as f:
-            c_num=f.readlines()[4]
-        with open(file) as f:
-            c_denom=f.readlines()[5]
+        a_num=int(lignes[0])
+        a_denom=int(lignes[1])
+        b_num=int(lignes[2])
+        b_denom=int(lignes[3])
+        c_num=int(lignes[4])
+        c_denom=int(lignes[5])
         prgm.ValeurA_n.set(a_num)
         prgm.ValeurA_d.set(a_denom)
         prgm.ValeurB_n.set(b_num)
