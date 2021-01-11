f34e0c090195912ee988d5efb7d9d21e3fa0ee41
rfoxinter <51450425+rfoxinter@users.noreply.github.com>
2021-01-11 21:44:41 +0100

    Update a_propos.py

--- EquationsSecondDegre/Appli/a_propos.py
+++ EquationsSecondDegre/Appli/a_propos.py
@@ -9,7 +9,7 @@ def main():
     prgm.about_root=Tk()
     prgm.about_root.option_add('*Font', 'Arial 10')
     Label(prgm.about_root,text='Version actuelle\u00A0:\u00A0'+str(prgm.version)).grid(column=0,row=0,sticky='w')
-    Label(prgm.about_root,text='-\u00A0Compatibile avec Linux.').grid(column=0,row=1,sticky='w')
+    Label(prgm.about_root,text='-\u00A0Am\u00E9lioration du script ouvrir.').grid(column=0,row=1,sticky='w')
     Label(prgm.about_root,width=50).grid(column=0,row=9)
     bouton_fermer=Button(prgm.about_root,text="Fermer",command=quitter.about_quit)
     bouton_fermer.grid(column=0,row=10)
