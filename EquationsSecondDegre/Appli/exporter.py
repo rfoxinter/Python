3d564778144c358745770d7a9f28bca944831990
rfoxinter <51450425+rfoxinter@users.noreply.github.com>
2021-01-16 12:58:28 +0100

    Update exporter.py

--- EquationsSecondDegre/Appli/exporter.py
+++ EquationsSecondDegre/Appli/exporter.py
@@ -4,7 +4,7 @@ import prgm
 from extensions import *
 
 def main(event=None):
-    file_list=[('Fichier texte','.txt .text'),('Fichier Rich Text Format','.rtf'),('Fichier Hypertext Markup Language','.html .htm'),('Fichier LaTeX','.tex'),('Tous les fichiers','.*')]
+    file_list=[('Fichier texte','.txt .text'),('Fichier Rich Text Format','.rtf'),('Fichier Document Office Open XML','.docx'),('Fichier HyperText Markup Language','.html .htm'),('Fichier Extensible HyperText Markup Language','.xhtml'),('Fichier LaTeX','.tex'),('Tous les fichiers','.*')]
     file=asksaveasfile(title='Enregistrer l\u2019\u00E9quation',filetypes=file_list,defaultextension='.txt')
     if file is None:
         return
@@ -121,8 +121,12 @@ def main(event=None):
         prgm.rep[0]='('+str(prgm.fraction_val[0])+')/('+str(prgm.fraction_val[1])+')x\u00B2+('+str(prgm.fraction_val[2])+')/('+str(prgm.fraction_val[3])+')x+('+str(prgm.fraction_val[4])+')/('+str(prgm.fraction_val[5])+')'
     if  pathlib.Path(file.name).suffix=='.rtf':
         rtf.main(file.name)
+    elif  pathlib.Path(file.name).suffix=='.docx':
+        docx.main(file.name.replace(pathlib.Path(file.name).suffix,''))
     elif pathlib.Path(file.name).suffix=='.html' or pathlib.Path(file.name).suffix=='.htm':
         html.main(file.name)
+    elif pathlib.Path(file.name).suffix=='.xhtml':
+        xhtml.main(file.name)
     elif pathlib.Path(file.name).suffix=='.tex':
         tex.main(file.name)
     else:
