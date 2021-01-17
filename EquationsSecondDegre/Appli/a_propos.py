ada407255d7a597677bce067427e8c2d42b698ec
rfoxinter <51450425+rfoxinter@users.noreply.github.com>
2021-01-17 21:58:54 +0100

    Update a_propos.py

--- EquationsSecondDegre/Appli/a_propos.py
+++ EquationsSecondDegre/Appli/a_propos.py
@@ -2,17 +2,17 @@ from tkinter import Tk,Label,Button
 import os
 import urllib.request
 import prgm
-import quitter
 
 #Close app
 def main():
     prgm.about_root=Tk()
     prgm.about_root.option_add('*Font', 'Arial 10')
     Label(prgm.about_root,text='Version actuelle\u00A0:\u00A0'+str(prgm.version)).grid(column=0,row=0,sticky='w')
-    Label(prgm.about_root,text='-\u00A0Am\u00E9lioration de la compatibilit\u00E9 avec linux.').grid(column=0,row=1,sticky='w')
-    Label(prgm.about_root,text='-\u00A0Am\u00E9lioration de l\u2019installation des packages.').grid(column=0,row=2,sticky='w')
+    Label(prgm.about_root,text='-\u00A0Correction de l\u2019icone Windows').grid(column=0,row=1,sticky='w')
+    Label(prgm.about_root,text='-\u00A0Am\u00E9lioration de l\u2019exportation au format .docx').grid(column=0,row=2,sticky='w')
+    Label(prgm.about_root,text='-\u00A0Am\u00E9lioration de la mise \u00E0 jour des pr\u00E9f\u00E9rences').grid(column=0,row=3,sticky='w')
     Label(prgm.about_root,width=50).grid(column=0,row=9)
-    bouton_fermer=Button(prgm.about_root,text="Fermer",command=quitter.about_quit)
+    bouton_fermer=Button(prgm.about_root,text="Fermer",command=prgm.about_root.destroy)
     bouton_fermer.grid(column=0,row=10)
     prgm.about_root.title("\u00C0 propos")
     prgm.about_root.resizable(width=False,height=False)
