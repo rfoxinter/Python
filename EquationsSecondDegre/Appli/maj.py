9a611c4baa6af81c706d20cffc2c193b34a7ae2f
rfoxinter <51450425+rfoxinter@users.noreply.github.com>
2021-01-17 21:59:41 +0100

    Update maj.py

--- EquationsSecondDegre/Appli/maj.py
+++ EquationsSecondDegre/Appli/maj.py
@@ -10,6 +10,7 @@ def maj_pref():
     maj_pref=open('preferences_maj.py','r')
     maj_lines=maj_pref.readlines()
     if len(local_lines)<len(maj_lines):
+        del local_lines[len(local_lines)]
         for i in range(len(maj_lines)-len(local_lines)):
             local_lines.append(maj_lines[len(local_lines)+i])
             local_pref=open(r'preferences.py','w')
@@ -20,7 +21,7 @@ def maj_pref():
 
 def down():
     extension=['.py','.ico','.png','.xml','.rels']
-    filename=[['a_propos','entier','entier_deux_racines_plt','entier_pas_racine','entier_x0','entier_x1','entier_x2','exporter','fraction','fraction_deux_racines_plt','fraction_pas_racine','fraction_x0','fraction_x1','fraction_x1_delta_denom_entier','fraction_x1_delta_num_entier','fraction_x1_sinon','fraction_x2','linux_distribution','maj','menu','ouvrir','packages','packages_linux','prgm','quitter','sauvegarder','version','extensions/__init__','extensions/docx','extensions/html','extensions/rtf','extensions/tex','extensions/txt','extensions/xhtml'],['a_propos','avertissement','package','python'],['python','package'],['extensions/docx/[Content_Types]','extensions/docx/numbering','extensions/docx/styles'],['extensions/docx/','extensions/docx/document.xml']]
+    filename=[['a_propos','edit_pref','entier','entier_deux_racines_plt','entier_pas_racine','entier_x0','entier_x1','entier_x2','exporter','fraction','fraction_deux_racines_plt','fraction_pas_racine','fraction_x0','fraction_x1','fraction_x1_delta_denom_entier','fraction_x1_delta_num_entier','fraction_x1_sinon','fraction_x2','linux_distribution','maj','menu','ouvrir','packages','packages_linux','prgm','quitter','sauvegarder','version','extensions/__init__','extensions/docx','extensions/html','extensions/rtf','extensions/tex','extensions/txt','extensions/xhtml'],['a_propos','avertissement','python'],['python','package'],['extensions/docx/[Content_Types]','extensions/docx/numbering','extensions/docx/styles'],['extensions/docx/','extensions/docx/document.xml']]
     try:
         if os.path.exists('extensions')==False:
             os.mkdir('extensions')
@@ -32,6 +33,12 @@ def down():
                 url='https://raw.githubusercontent.com/rfoxinter/Python/master/EquationsSecondDegre/Appli/'+filename[k][i]+extension[k]
                 urllib.request.urlretrieve(url,filename[k][i]+extension[k])
         maj_pref()
+        import edit_pref
+        import importlib
+        importlib.reload(edit_pref)
+        edit_pref.main('etat_maj','L\u2019application a bien \u00E9t\u00E9 mise \u00E0 jour.',2)
     except:
-        import prgm
-        Label(prgm.maj_root,text='Une mise \u00E0 jour est disponible.',width=50).grid(column=0,row=0)
+        import edit_pref
+        import importlib
+        importlib.reload(edit_pref)
+        edit_pref.main('etat_maj','Une erreur est survenue lors de la mise \u00E0 jour.',2)
