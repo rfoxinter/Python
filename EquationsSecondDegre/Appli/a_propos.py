8e630272c8becb499fc8126cb1df9a05b2f0bd31
rfoxinter <51450425+rfoxinter@users.noreply.github.com>
2021-01-23 12:27:38 +0100

    Update a_propos.py

--- EquationsSecondDegre/Appli/a_propos.py
+++ EquationsSecondDegre/Appli/a_propos.py
@@ -8,7 +8,7 @@ def main():
     prgm.about_root=Tk()
     prgm.about_root.option_add('*Font', 'Arial 10')
     Label(prgm.about_root,text='Version actuelle\u00A0:\u00A0'+str(prgm.version)).grid(column=0,row=0,sticky='w')
-    Label(prgm.about_root,text='-\u00A0Am\u00E9lioration de l\u2019icone Windows').grid(column=0,row=1,sticky='w')
+    Label(prgm.about_root,text='-\u00A0Ajout d\u2019icones dans le menu').grid(column=0,row=1,sticky='w')
     Label(prgm.about_root,width=50).grid(column=0,row=9)
     bouton_fermer=Button(prgm.about_root,text="Fermer",command=prgm.about_root.destroy)
     bouton_fermer.grid(column=0,row=10)
