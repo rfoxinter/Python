8c1c6d9b7bcb97055629be4df5c0d3d8c93a7b3b
rfoxinter <51450425+rfoxinter@users.noreply.github.com>
2021-06-28 15:15:59 +0200

    Create erreur.py

---
+++ EquationsSecondDegre/Appli/erreur.py
@@ -0,0 +1,13 @@
+import prgm
+import preferences
+
+def main():
+    if preferences.afficher_graphs==1:
+        prgm.plt.close()
+    for label in prgm.root.grid_slaves():
+        if int(label.grid_info()['column'])==6:
+            label.destroy()
+    Col7=prgm.Label(prgm.root,width=100)
+    Col7.grid(column=6,row=5)
+    L1=prgm.Label(prgm.root,text='Une erreur est survenue lors de l\u2019ouverture du fichier')
+    L1.grid(column=6,row=0,sticky='w')
