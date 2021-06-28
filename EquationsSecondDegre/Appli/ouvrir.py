eaeb3df7d4cbb9a570e0ec037419035512167982
rfoxinter <51450425+rfoxinter@users.noreply.github.com>
2021-06-28 15:16:12 +0200

    Update ouvrir.py

--- EquationsSecondDegre/Appli/ouvrir.py
+++ EquationsSecondDegre/Appli/ouvrir.py
@@ -2,12 +2,13 @@ import prgm
 from tkinter.filedialog import askopenfilename
 import entier
 import fraction
+import erreur
 
 def main(event=None):
     file=askopenfilename(title='S\u00E9lectionner un fichier à ouvrir',filetypes=[('Fichier \u00E9quation', '.eq')],defaultextension='.eq')
     lire=open(file,'r')
     lignes=lire.readlines()
-    if len(lignes)<6:
+    if len(lignes)==3:
         a=int(lignes[0])
         b=int(lignes[1])
         c=int(lignes[2])
@@ -15,7 +16,7 @@ def main(event=None):
         prgm.ValeurB.set(b)
         prgm.ValeurC.set(b)
         entier.open_ent(a,b,c)
-    else:
+    elif len(lignes)==6:
         a_num=int(lignes[0])
         a_denom=int(lignes[1])
         b_num=int(lignes[2])
@@ -29,3 +30,5 @@ def main(event=None):
         prgm.ValeurC_n.set(c_num)
         prgm.ValeurC_d.set(c_denom)
         fraction.open_frac(int(a_num),int(a_denom),int(b_num),int(b_denom),int(c_num),int(c_denom))
+    else:
+        erreur.main()
