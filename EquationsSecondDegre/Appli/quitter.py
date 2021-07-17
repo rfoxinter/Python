5b47e05d049577b7e77ab4d1657a93780f8c066d
rfoxinter <51450425+rfoxinter@users.noreply.github.com>
2021-07-17 15:47:05 +0200

    Update quitter.py

--- EquationsSecondDegre/Appli/quitter.py
+++ EquationsSecondDegre/Appli/quitter.py
@@ -4,7 +4,6 @@ import preferences
 import __init__
 
 def download():
-    #Download version file
     url='https://raw.githubusercontent.com/rfoxinter/Python/master/EquationsSecondDegre/Appli/version.py'
     urllib.request.urlretrieve(url,'version.py')
 
@@ -34,7 +33,6 @@ def close(event=None):
         if 'DejaVu Sans' in list(prgm.font.families()):
             police=prgm.font.Font(root,family='DejaVu Sans',size=10)
             root.option_add('*Font',police)
-        #Check for a new version
         if version.version>prgm.version:
             url='https://raw.githubusercontent.com/rfoxinter/Python/master/EquationsSecondDegre/Appli/maj.py'
             urllib.request.urlretrieve(url,'maj.py')
@@ -55,26 +53,40 @@ def close(event=None):
             root.destroy()
 
 def abt_maj():
-    download()
-    prgm.maj_root=prgm.Tk()
-    if 'DejaVu Sans' in list(prgm.font.families()):
-        police=prgm.font.Font(prgm.maj_root,family='DejaVu Sans',size=10)
-        prgm.maj_root.option_add('*Font',police)
-    import version
-    if version.version>prgm.version:
-        url='https://raw.githubusercontent.com/rfoxinter/Python/master/EquationsSecondDegre/Appli/maj.py'
-        urllib.request.urlretrieve(url,'maj.py')
-        prgm.Label(prgm.maj_root,text='Une mise \u00E0 jour est disponible.',width=50).grid(column=0,row=0)
-        bouton_maj=prgm.Button(prgm.maj_root,text='Mettre \u00E0 jour l\u2019application',command=__init__.mise_a_jour)
-        bouton_maj.grid(column=0,row=2)
-        bouton_fermer=prgm.Button(prgm.maj_root,text='Ne pas mettre à jour',command=prgm.maj_root.destroy)
-        bouton_fermer.grid(column=0,row=3)
-    else:
-        prgm.Label(prgm.maj_root,text='L\u2019application est \u00E0 jour.',width=50).grid(column=0,row=0)
+    try:
+        download()
+        prgm.maj_root=prgm.Tk()
+        if 'DejaVu Sans' in list(prgm.font.families()):
+            police=prgm.font.Font(prgm.maj_root,family='DejaVu Sans',size=10)
+            prgm.maj_root.option_add('*Font',police)
+        import version
+        if version.version>prgm.version:
+            url='https://raw.githubusercontent.com/rfoxinter/Python/master/EquationsSecondDegre/Appli/maj.py'
+            urllib.request.urlretrieve(url,'maj.py')
+            prgm.Label(prgm.maj_root,text='Une mise \u00E0 jour est disponible.',width=50).grid(column=0,row=0)
+            bouton_maj=prgm.Button(prgm.maj_root,text='Mettre \u00E0 jour l\u2019application',command=__init__.mise_a_jour)
+            bouton_maj.grid(column=0,row=2)
+            bouton_fermer=prgm.Button(prgm.maj_root,text='Ne pas mettre à jour',command=prgm.maj_root.destroy)
+            bouton_fermer.grid(column=0,row=3)
+        else:
+            prgm.Label(prgm.maj_root,text='L\u2019application est \u00E0 jour.',width=50).grid(column=0,row=0)
+            bouton_fermer=prgm.Button(prgm.maj_root,text='Fermer',command=prgm.maj_root.destroy)
+            bouton_fermer.grid(column=0,row=2)
+        prgm.maj_root.title('Mise \u00E0 jour')
+        prgm.maj_root.resizable(width=False,height=False)
+        if prgm.os.name=='nt':
+            prgm.maj_root.iconbitmap('python.ico')
+        prgm.maj_root.mainloop()
+    except:
+        prgm.maj_root=prgm.Tk()
+        if 'DejaVu Sans' in list(prgm.font.families()):
+            police=prgm.font.Font(prgm.maj_root,family='DejaVu Sans',size=10)
+            prgm.maj_root.option_add('*Font',police)
+        prgm.Label(prgm.maj_root,text='Impossible de v\u00E9rifier les mises \u00E0 jour.',width=50).grid(column=0,row=0)
         bouton_fermer=prgm.Button(prgm.maj_root,text='Fermer',command=prgm.maj_root.destroy)
         bouton_fermer.grid(column=0,row=2)
-    prgm.maj_root.title('Mise \u00E0 jour')
-    prgm.maj_root.resizable(width=False,height=False)
-    if prgm.os.name=='nt':
-        prgm.maj_root.iconbitmap('python.ico')
-    prgm.maj_root.mainloop()
+        prgm.maj_root.title('Mise \u00E0 jour')
+        prgm.maj_root.resizable(width=False,height=False)
+        if prgm.os.name=='nt':
+            prgm.maj_root.iconbitmap('python.ico')
+        prgm.maj_root.mainloop()
