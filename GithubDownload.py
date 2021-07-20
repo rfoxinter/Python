7ea528351db2b5a51d82d30835b9b848b130798c
rfoxinter <51450425+rfoxinter@users.noreply.github.com>
2021-07-20 14:37:24 +0200

    Add files via upload

--- GithubDownload.py
+++ GithubDownload.py
@@ -1,11 +1,23 @@
 import urllib.request
 from urllib.parse import quote
-from os.path import exists
+from os.path import dirname,exists
 from os import mkdir
+from time import sleep
+
+print('GithubDownload v2\n')
 
 url=input('URL du dossier : ')
+
+print('')
+
+src=urllib.request.urlopen(url).read().decode('utf-8')
 if not '/tree' in url:
-    url=(url+'/tree/master').replace('//t','/t')
+    branch=''
+    src=src[src.find('data-menu-button')+17:len(src)]
+    while src[0]!='<':
+        branch=branch+src[0]
+        src=src[1:len(src)]
+    url=(url+'/tree/'+branch).replace('//t','/t')
 if url[len(url)-1]=='/':
     url=url[0:len(url)-1]
 files=[]
@@ -39,10 +51,16 @@ def analyze(code):
         elif _type=='D':
             folders.append('https://github.com'+href)
 
-analyze(urllib.request.urlopen(url).read().decode('utf-8'))
+analyze(src)
+
+def check_parent(path):
+    if not exists(dirname(path)):
+        check_parent(dirname(path))
+        mkdir(dirname(path))
 
 while len(folders)!=0:
     if not exists(folder+'/'+folders[0].replace(url+'/','')):
+        check_parent(folder+'/'+folders[0].replace(url+'/',''))
         mkdir(folder+'/'+folders[0].replace(url+'/',''))
     analyze(urllib.request.urlopen(folders[0]).read().decode('utf-8'))
     folders.pop(0)
@@ -51,8 +69,12 @@ path=url.replace('https://github.com','https://raw.githubusercontent.com')
 path=path.replace('tree/','')
 
 for i in files:
+    print('Téléchargement en cours ('+str(files.index(i)+1)+'/'+str(len(files))+')')
     i=(i.encode('latin1')).decode('utf-8')
     f=i
     f=quote(f)
     f=f.replace('%3A//','://')
     urllib.request.urlretrieve(f,folder+'/'+i.replace(path,''))
+
+print('\nTéléchargement terminé')
+sleep(5)
