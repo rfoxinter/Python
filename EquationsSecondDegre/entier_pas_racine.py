e1fa873ed0f44c51e0bbfcb525b840f879eb24ca
rfoxinter <51450425+rfoxinter@users.noreply.github.com>
2021-12-19 17:39:10 +0100

    Update entier_pas_racine.py

--- EquationsSecondDegre/entier_pas_racine.py
+++ EquationsSecondDegre/entier_pas_racine.py
@@ -9,7 +9,7 @@ def main(a,b,c,alpha_num,alpha_denom,beta_num,beta_denom,extension,path):
         prgm.plt.rcParams['pdf.fonttype']=42
         prgm.plt.rcParams['font.family']='DejaVu Sans'
         prgm.plt.rcParams['font.size']='10'
-    L4=prgm.Label(prgm.root,text='L\u2019\u00E9quation n\u2019admet pas de racine.')
+    L4=prgm.Label(prgm.root,text='L\u2019\u00E9quation n\u2019admet pas de racine dans \u211D.')
     L4.grid(column=6,row=3,sticky='w')
     prgm.rep[5]=L4.cget('text')
     if preferences.afficher_graphs==1 or extension!='':
