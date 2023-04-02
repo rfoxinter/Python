a29e35810115e6f5dab1d932365da8e057431e92
rfoxinter <rfoxinter@gmail.com>
2023-04-02 19:08:05 +0200

    Update a_propos.py

--- EquationsSecondDegre/a_propos.py
+++ EquationsSecondDegre/a_propos.py
@@ -6,7 +6,7 @@ def main():
         police=prgm.font.Font(prgm.about_root,family='DejaVu Sans',size=10)
         prgm.about_root.option_add('*Font',police)
     prgm.Label(prgm.about_root,text='Version actuelle\u00A0:\u00A0'+str(prgm.version)).grid(column=0,row=0,sticky='w')
-    prgm.Label(prgm.about_root,text='-\u00A0Am\u00E9lioration de l\u2019affichage des r\u00E9sultats ').grid(column=0,row=1,sticky='w')
+    prgm.Label(prgm.about_root,text='-\u00A0Améloration de l\u2019exportation en LATEX').grid(column=0,row=1,sticky='w')
     prgm.Label(prgm.about_root,width=50).grid(column=0,row=9)
     bouton_fermer=prgm.Button(prgm.about_root,text="Fermer",command=prgm.about_root.destroy)
     bouton_fermer.grid(column=0,row=10)
