ac47128c8b2f8edf0642c94d77b4bacb104fac8a
rfoxinter <rfoxinter@gmail.com>
2026-05-20 17:27:40 +0200

    Update mult.py

--- pdf_toolkit/scripts/mult.py
+++ pdf_toolkit/scripts/mult.py
@@ -1,4 +1,4 @@
-from tchar_py.numbers.rational import r, itor
+from tchar_py.numbers.rational import rat
 from pypdf import PdfReader, PdfWriter, Transformation, PageObject, PaperSize
 
 def land(size):
@@ -10,7 +10,7 @@ def ord2(n: int) -> range:
 def ord4(n: int) -> range:
     return range((n + 1) // 2)
 
-def ord2land(n: int) -> range:
+def ord2land(n: int) -> list[int]:
     def aux(x):
         if x % 4 == 1:
             return x + 1
@@ -19,24 +19,12 @@ def ord2land(n: int) -> range:
         return x
     return [aux(i) for i in range((n + 3) // 4 * 4)]
 
-def mult(inp: PdfReader, pgord: range, le: bool, pw: r, ph: r, l: int, island: bool) -> PdfWriter:
+def mult(inp: PdfReader, pgord: range, le: bool, pw: rat, ph: rat, l: int, island: bool) -> PdfWriter:
     out = PdfWriter()
     for pos, pg in enumerate(pgord):
         page = inp.pages[pg] if pg < l else PageObject.create_blank_page(width = float(pw), height = float(ph))
-        h = page.mediabox.height
-        if isinstance(h, int):
-            h = itor(h)
-        else:
-            d = 10 ** (len(str(h)) - len(str(int(h))) - 1)
-            h = r(h * d, d)
-            del d
-        w = page.mediabox.width
-        if isinstance(w, int):
-            w = itor(w)
-        else:
-            d = 10 ** (len(str(w)) - len(str(int(w))) - 1)
-            w = r(w * d, d)
-            del d
+        h = rat(page.mediabox.height)
+        w = rat(page.mediabox.width)
         if pw > ph:
             dx, dy, ppw, pph = pw / 2 * (pos % 2), 0, pw / 2, ph
         else:
@@ -50,7 +38,7 @@ def mult(inp: PdfReader, pgord: range, le: bool, pw: r, ph: r, l: int, island: b
             npage.merge_transformed_page(page, tr)
         else:
             npage.merge_transformed_page(page, tr)
-            if le and pos % 4 == 3:
+            if not le and pos % 4 == 3:
                 npage.rotation = 180
             out.add_page(npage)
     return out
@@ -59,18 +47,18 @@ def mult2(flnm: str, size: str, flip: str):
     le = flip == 'Long edge'
     inp = PdfReader(flnm)
     l = len(inp.pages)
-    pw = itor(getattr(PaperSize, size).width)
-    ph = itor(getattr(PaperSize, size).height)
+    pw = rat(getattr(PaperSize, size).width)
+    ph = rat(getattr(PaperSize, size).height)
     return mult(inp, ord2(l), le, pw, ph, l, size[0:4] == 'Land')
 
 def mult4(flnm: str, size: str, flip: str):
     le = flip == 'Long edge'
     inp = PdfReader(flnm)
     l = len(inp.pages)
-    pw = itor(getattr(PaperSize, size).width)
-    ph = itor(getattr(PaperSize, size).height)
-    p2w = itor(getattr(PaperSize, land(size)).width)
-    p2h = itor(getattr(PaperSize, land(size)).height)
+    pw = rat(getattr(PaperSize, size).width)
+    ph = rat(getattr(PaperSize, size).height)
+    p2w = rat(getattr(PaperSize, land(size)).width)
+    p2h = rat(getattr(PaperSize, land(size)).height)
     if p2w < p2h:
         pgord2 = ord2land(l)
     else:
